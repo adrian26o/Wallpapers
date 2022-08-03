29038238fccaa29af05749461f3aa2e673fc00d0
adrian26o <adrianfloresnoh@gmail.com>
2022-08-02 23:17:21 -0500

    test

--- generateJSON.js
+++ generateJSON.js
@@ -5,14 +5,14 @@ const assetsDir =  path.join(__dirname, "assets");
 let assetsFolder = fs.readdirSync(assetsDir);
 const outputFile = "./data.json"
 
-const BaseUrl = "https://github.com/adrian26o/Wallpapers";
+const BaseUrl = "https://adrian26o.github.io/Wallpapers";
 
 const folderUrl = "assets";
 
 assetsFolder = assetsFolder.map(v => ({
     "name": v.split(".").slice(0,v.split(".").length - 1).join("."),
     "ext": path.extname(v),
-    "url": `${BaseUrl}/blob/main/${folderUrl}/${v}?raw=true`
+    "url": `${BaseUrl}/blob/main/${folderUrl}/${v}`
 })
 )
 
